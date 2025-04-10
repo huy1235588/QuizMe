f3f6b89581148116c6a7d1565572ac4317af0c6f
huy1235588 <viettien4869@gmail.com>
2025-04-10 20:18:30 +0700

    first commit

---
+++ app/src/main/java/com/huy/QuizMe/MainActivity.java
@@ -0,0 +1,54 @@
+package com.huy.QuizMe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.huy.QuizMe.databinding.ActivityMainBinding;
+import com.huy.QuizMe.fragments.HomeFragment;
+import com.huy.QuizMe.fragments.LibraryFragment;
+import com.huy.QuizMe.fragments.ProfileFragment;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Khởi tạo Fragment HomeFragment
+        HomeFragment homeFragment = new HomeFragment();
+        // Thay thế Fragment mặc định
+        replaceFragment(homeFragment);
+
+        // Thiết lập sự kiện cho các nút điều hướng
+        binding.bottomNav.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            // Thay thế Fragment dựa trên nút được chọn
+            if (itemId == R.id.nav_home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.nav_library) {
+                replaceFragment(new LibraryFragment());
+            } else if (itemId == R.id.nav_profile) {
+                replaceFragment(new ProfileFragment());
+            } else {
+                return false;
+            }
+
+            return true;
+        });
+    }
+
+    /**
+     * Thay thế Fragment hiện tại bằng Fragment mới
+     * @param fragment Fragment mới để hiển thị
+     */
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frm_container, fragment)
+                .commit();
+    }
+}
---
+++ app/src/main/java/com/huy/QuizMe/fragments/LibraryFragment.java
@@ -0,0 +1,29 @@
+package com.huy.QuizMe.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.huy.QuizMe.R;
+
+
+public class LibraryFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_library, container, false);
+
+        return view;
+    }
+}
